8c56944f2332d37856859ac53e9b80923a02531a
ThomasMartinod <110356084+ThomasMartinod@users.noreply.github.com>
2022-08-16 21:57:27 -0500

    Add files via upload

---
+++ codigo/Code V1.0.py
@@ -0,0 +1,13 @@
+import geopandas as gpd
+import pandas as pd
+from shapely import wkt
+
+city = pd.read_csv('poligono_de_medellin.csv',sep=';')
+city['geometry'] = city['geometry'].apply(wkt.loads)    # Deletes bad geometries
+city = gpd.GeoDataFrame(city)
+
+data = pd.read_csv("calles_de_medellin_con_acoso.csv",sep=';')
+data['geometry'] = data['geometry'].apply(wkt.loads)
+edges = gpd.GeoDataFrame(data)
+
+# The data is saved as a Pandas DataFrame :)
